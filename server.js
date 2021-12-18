e018149395998ec28360dade3f59e71a188c5be9
fizikci <bulentkeskin@gmail.com>
2021-12-17 22:36:34 -0800

    added smart home endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('static'));
 
+app.get('/smart-home-keskin', (req,res)=>{
+    res.send('hello');
+});
+
 app.listen(3000, () => {
     console.log('App started on port 3000');
 });

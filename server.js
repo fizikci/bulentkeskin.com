6d0585b431a53c8804d4ca2f18d9d1bd13c58a0b
fizikci <bulentkeskin@gmail.com>
2021-12-17 23:28:10 -0800

    added smart home request log

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs')
 
 const app = express();
 app.use(express.static('static'));
 
 app.get('/smart-home-keskin', (req,res)=>{
+
+    const content = new Date() + req.hostname+'\n\n'+JSON.stringify(req.body)+'\n\n'
+
+    fs.writeFile('./http.log', content, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      //file written successfully
+    })
+
     res.send('hello');
 });
 
